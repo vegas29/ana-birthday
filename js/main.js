ec9ebdaca9d0b4e7edf2f4951b6ce0676c91e62f
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-11-29 16:41:40 -0500

    Birthday ready

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+    $('.cuenta-regresiva').countdown('2022/07/11 00:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+      });
